818a6aa6e8d2a5af0f33b6be85ac3ede00f5ece7
Sakthi <sakthi1996sm@gmail.com>
2025-11-29 12:06:00 +0530

    Fix: Support POSTGRES_URL env var for Vercel Postgres

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -81,7 +81,7 @@ import dj_database_url
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
+        default=os.environ.get('POSTGRES_URL', 'sqlite:///' + str(BASE_DIR / 'db.sqlite3')),
         conn_max_age=600
     )
 }
