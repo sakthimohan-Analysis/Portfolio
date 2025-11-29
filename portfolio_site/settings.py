8460acf2fb8b036b46aed6537965f8b0f0170980
Sakthi <sakthi1996sm@gmail.com>
2025-11-29 12:03:46 +0530

    Fix database connection: Use dj-database-url for Vercel Postgres support

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -77,11 +77,13 @@ WSGI_APPLICATION = 'portfolio_site.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
+        conn_max_age=600
+    )
 }
 
 
